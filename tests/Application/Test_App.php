8da0522e10515e2c85a8f8b9056fc5b91c7419da
gin0115 <glynn.quelch@gmail.com>
2021-08-09 14:35:47 +0100

    House Keeping

--- tests/Application/Test_App.php
+++ tests/Application/Test_App.php
@@ -29,241 +29,219 @@ use PinkCrab\Perique\Exceptions\App_Initialization_Exception;
 use PinkCrab\Perique\Services\Registration\Registration_Service;
 use PinkCrab\Perique\Tests\Fixtures\Mock_Objects\Mock_Registration_Middleware;
 
-class Test_App extends WP_UnitTestCase
-{
+class Test_App extends WP_UnitTestCase {
+
 
 	/**
 	 * @method self::unset_app_instance();
 	 */
 	use App_Helper_Trait;
 
-	public function tearDown(): void
-	{
+	public function tearDown(): void {
 		self::unset_app_instance();
 	}
 
 	/** @testdox When a container is passed to the application, it should be set as an internal property of the app. */
-	public function test_set_container(): void
-	{
+	public function test_set_container(): void {
 		$app       = new App();
-		$container = $this->createMock(DI_Container::class);
-		$app->set_container($container);
-		$this->assertSame($container, Objects::get_property($app, 'container'));
+		$container = $this->createMock( DI_Container::class );
+		$app->set_container( $container );
+		$this->assertSame( $container, Objects::get_property( $app, 'container' ) );
 	}
 
 	/** @testdox The app should only allow one container to set, attempting to set another should cause the process to fail. */
-	public function test_set_container_exception(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(2);
+	public function test_set_container_exception(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 2 );
 
 		$app       = new App();
-		$container = $this->createMock(DI_Container::class);
-		$app->set_container($container);
-		$app->set_container($container);
+		$container = $this->createMock( DI_Container::class );
+		$app->set_container( $container );
+		$app->set_container( $container );
 	}
 
 	/** @testdox A set of configs for the application can be bound as App_Config */
-	public function test_set_app_config(): void
-	{
+	public function test_set_app_config(): void {
 		$app = new App();
-		$app->set_app_config(array());
-		$this->assertInstanceOf(App_Config::class, Objects::get_property($app, 'app_config'));
+		$app->set_app_config( array() );
+		$this->assertInstanceOf( App_Config::class, Objects::get_property( $app, 'app_config' ) );
 	}
 
 	/** @testdox The applications config should only be settable once attempting to set another should cause the process to fail. */
-	public function test_set_app_config_exception(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(5);
+	public function test_set_app_config_exception(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 5 );
 
 		$app = new App();
-		$app->set_app_config(array());
-		$app->set_app_config(array());
+		$app->set_app_config( array() );
+		$app->set_app_config( array() );
 	}
 
 	/** @testdox The registration service should be setable and bound to the registarion property */
-	public function test_set_registration_services(): void
-	{
+	public function test_set_registration_services(): void {
 		$app          = new App();
-		$registration = $this->createMock(Registration_Service::class);
-		$app->set_registration_services($registration);
-		$this->assertSame($registration, Objects::get_property($app, 'registration'));
+		$registration = $this->createMock( Registration_Service::class );
+		$app->set_registration_services( $registration );
+		$this->assertSame( $registration, Objects::get_property( $app, 'registration' ) );
 	}
 
 	/** @testdox The applications registration service should only be settable once, attempting to set another should cause the process to fail. */
-	public function test_set_registration_services_exception(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(7);
+	public function test_set_registration_services_exception(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 7 );
 
 		$app          = new App();
-		$registration = $this->createMock(Registration_Service::class);
-		$app->set_registration_services($registration);
-		$app->set_registration_services($registration);
+		$registration = $this->createMock( Registration_Service::class );
+		$app->set_registration_services( $registration );
+		$app->set_registration_services( $registration );
 	}
 
 	/** @testdox The loader should be setable and bound to the loader property */
-	public function test_set_loader(): void
-	{
+	public function test_set_loader(): void {
 		$app    = new App();
-		$loader = $this->createMock(Hook_Loader::class);
-		$app->set_loader($loader);
-		$this->assertSame($loader, Objects::get_property($app, 'loader'));
+		$loader = $this->createMock( Hook_Loader::class );
+		$app->set_loader( $loader );
+		$this->assertSame( $loader, Objects::get_property( $app, 'loader' ) );
 	}
 
 	/** @testdox The applications loader should only be settable once, attempting to set another should cause the process to fail. */
-	public function test_set_loader_exception(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(8);
+	public function test_set_loader_exception(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 8 );
 
 		$app    = new App();
-		$loader = $this->createMock(Hook_Loader::class);
-		$app->set_loader($loader);
-		$app->set_loader($loader);
+		$loader = $this->createMock( Hook_Loader::class );
+		$app->set_loader( $loader );
+		$app->set_loader( $loader );
 	}
 
 	/** @testdox The applications container should have an access point so custom rules can be added before the app is booted. */
-	public function test_container_config(): void
-	{
+	public function test_container_config(): void {
 		$app       = new App();
-		$container = $this->createMock(DI_Container::class);
-		$app->set_container($container);
+		$container = $this->createMock( DI_Container::class );
+		$app->set_container( $container );
 		$app->container_config(
-			function (DI_Container $container): void {
-				$this->assertInstanceOf(DI_Container::class, $container);
+			function ( DI_Container $container ): void {
+				$this->assertInstanceOf( DI_Container::class, $container );
 			}
 		);
 	}
 
 	/** @testdox Trying to configure the container before its set should result in an error and ending the intialisation. */
-	public function test_container_config_exception(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(1);
+	public function test_container_config_exception(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 1 );
 
 		$app = new App();
 		$app->container_config(
-			function (DI_Container $container): void {
-				$this->assertInstanceOf(DI_Container::class, $container);
+			function ( DI_Container $container ): void {
+				$this->assertInstanceOf( DI_Container::class, $container );
 			}
 		);
 	}
 
 	/** @testdox Additional functionality should be added at boot up through the means of middleware */
-	public function test_registration_middleware(): void
-	{
+	public function test_registration_middleware(): void {
 		$app          = new App();
 		$registration = new Registration_Service();
-		$middleware   = $this->createMock(Registration_Middleware::class);
+		$middleware   = $this->createMock( Registration_Middleware::class );
 
-		$app->set_registration_services($registration);
-		$app->registration_middleware($middleware);
-		$this->assertContains($middleware, Objects::get_property($registration, 'middleware'));
+		$app->set_registration_services( $registration );
+		$app->registration_middleware( $middleware );
+		$this->assertContains( $middleware, Objects::get_property( $registration, 'middleware' ) );
 	}
 
 	/** @testdox If middleware is added before the registation service has been bound to the app, the system should return an error. */
-	public function test_registration_middleware_exception(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(3);
+	public function test_registration_middleware_exception(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 3 );
 
 		$app        = new App();
-		$middleware = $this->createMock(Registration_Middleware::class);
-		$app->registration_middleware($middleware);
+		$middleware = $this->createMock( Registration_Middleware::class );
+		$app->registration_middleware( $middleware );
 	}
 
 	/** @testdox A list of classes which should be run through the registration process, should be able to stacked up ready to go. */
-	public function test_registration_classes(): void
-	{
+	public function test_registration_classes(): void {
 		$app          = new App();
 		$registration = new Registration_Service();
-		$app->set_registration_services($registration);
-		$app->registration_classes(array(Sample_Class::class));
-		$this->assertContains(Sample_Class::class, Objects::get_property($registration, 'class_list'));
+		$app->set_registration_services( $registration );
+		$app->registration_classes( array( Sample_Class::class ) );
+		$this->assertContains( Sample_Class::class, Objects::get_property( $registration, 'class_list' ) );
 	}
 
 	/** @testdox If classes are set for registration before the service has been bound to the application, it should error and abort initialisation. */
-	public function test_registration_classes_exception(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(3);
+	public function test_registration_classes_exception(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 3 );
 
 		$app = new App();
-		$app->registration_classes(array(Sample_Class::class));
+		$app->registration_classes( array( Sample_Class::class ) );
 	}
 
 	/** @testdox When a fully populated app is booted, it should pass valdaition and run all internal setups. */
-	public function test_boot(): void
-	{
+	public function test_boot(): void {
 		$app = $this->pre_populated_app_provider();
 
 		// Ensure app is not marked as booted before calling boot()
-		$this->assertFalse($app::is_booted());
+		$this->assertFalse( $app::is_booted() );
 
 		$app->boot();
 
 		// Check the app has been booted and container is bound to registration.
-		$this->assertTrue($app::is_booted());
-		$registration = Objects::get_property($app, 'registration');
+		$this->assertTrue( $app::is_booted() );
+		$registration = Objects::get_property( $app, 'registration' );
 		$this->assertInstanceOf(
 			DI_Container::class,
-			Objects::get_property($registration, 'di_container')
+			Objects::get_property( $registration, 'di_container' )
 		);
 	}
 
 	/** @testdox The app should only be bootable only once, trying to reboot should cause an error and abort the request. */
-	public function test_throws_exception_if_trying_to_boot_twice(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(6);
+	public function test_throws_exception_if_trying_to_boot_twice(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 6 );
 		$app = $this->pre_populated_app_provider();
 		$app->boot();
 		$app->boot();
 	}
 
 	/** @testdox The apps internal serives (View, DI & App_Config) can only be used once the application has been booted. */
-	public function test_throws_exception_if_view_is_called_before_app_booted(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(4);
+	public function test_throws_exception_if_view_is_called_before_app_booted(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 4 );
 		$app = $this->pre_populated_app_provider();
 		$app::view();
 	}
 
 	/** @testdox It should be possible to access the current DI container from the App instance, for use in additional libraies that need access. */
-	public function test_can_get_container_from_app(): void
-	{
+	public function test_can_get_container_from_app(): void {
 		$app = $this->pre_populated_app_provider();
-		$this->assertInstanceOf(DI_Container::class, $app->get_container());
+		$this->assertInstanceOf( DI_Container::class, $app->get_container() );
 	}
 
 	/** @testdox Attemptingt to access the DI Container before it has been defiend, should resutl in an Application Intialization exception. */
-	public function test_throws_exception_if_attempting_to_access_undefined_di_container(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(1);
+	public function test_throws_exception_if_attempting_to_access_undefined_di_container(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 1 );
 		$app = new App();
 		$app->get_container();
 	}
 
 
 	/** @testdox It should be possible to access App_Config using the Config Facade */
-	public function test_config_facade(): void
-	{
+	public function test_config_facade(): void {
 		$app = $this->pre_populated_app_provider();
 		$app->boot();
-		$this->assertEquals(FIXTURES_PATH . '/Views/', Config::path('view'));
-		$this->assertEquals('test_value', Config::additional('test_key'));
+		$this->assertEquals( FIXTURES_PATH . '/Views/', Config::path( 'view' ) );
+		$this->assertEquals( 'test_value', Config::additional( 'test_key' ) );
 	}
 
 	/** @testdox When trying to pass the class name for Registration Middleware, an exception should be thrown if container not defined. */
-	public function test_throws_exception_if_trying_to_construct_middleware_before_container_initialised(): void
-	{
-		$this->expectException(App_Initialization_Exception::class);
-		$this->expectExceptionCode(1);
-		$app = (new App())->set_registration_services(new Registration_Service());
-		$app->construct_registration_middleware(Mock_Registration_Middleware::class);
+	public function test_throws_exception_if_trying_to_construct_middleware_before_container_initialised(): void {
+		$this->expectException( App_Initialization_Exception::class );
+		$this->expectExceptionCode( 1 );
+		$app = ( new App() )->set_registration_services( new Registration_Service() );
+		$app->construct_registration_middleware( Mock_Registration_Middleware::class );
 	}
 }
--- tests/Application/Test_App_Factory.php
+++ tests/Application/Test_App_Factory.php
@@ -24,119 +24,110 @@ use PinkCrab\Perique\Tests\Fixtures\Mock_Objects\Has_DI_Container;
 use PinkCrab\Perique\Tests\Fixtures\Mock_Objects\Hookable\Hookable_Mock;
 use PinkCrab\Perique\Tests\Fixtures\Mock_Objects\Mock_Registration_Middleware;
 
-class Test_App_Factory extends WP_UnitTestCase
-{
+class Test_App_Factory extends WP_UnitTestCase {
+
 
 	/**
 	 * @method self::unset_app_instance();
 	 */
 	use App_Helper_Trait;
 
-	public function tearDown(): void
-	{
+	public function tearDown(): void {
 		self::unset_app_instance();
 	}
 
 	/** @testdox When requested the App Factory can create an instance of App popualted with WP_Dice, Hookables Middleware, Loader and Registration Service. */
-	public function test_can_create_with_wp_dicece(): void
-	{
-		$app = (new App_Factory)
-			->with_wp_dice(true)
+	public function test_can_create_with_wp_dicece(): void {
+		$app = ( new App_Factory )
+			->with_wp_dice( true )
 			->app();
 
-		$this->assertInstanceOf(App::class, $app);
+		$this->assertInstanceOf( App::class, $app );
 		$this->assertInstanceOf(
 			DI_Container::class,
-			Objects::get_property($app, 'container')
+			Objects::get_property( $app, 'container' )
 		);
 		$this->assertInstanceOf(
 			Hook_Loader::class,
-			Objects::get_property($app, 'loader')
+			Objects::get_property( $app, 'loader' )
 		);
 	}
 
 	/** @testdox A classes which need to be registered, should be passable at setup. Allowing plugins to register hooks with WordPress */
-	public function test_can_set_registration_classes(): void
-	{
-		$app = (new App_Factory)
-			->with_wp_dice(true)
-			->registration_classes(array(Hookable_Mock::class))->app();
+	public function test_can_set_registration_classes(): void {
+		$app = ( new App_Factory )
+			->with_wp_dice( true )
+			->registration_classes( array( Hookable_Mock::class ) )->app();
 
-		$registration_service = Objects::get_property($app, 'registration');
+		$registration_service = Objects::get_property( $app, 'registration' );
 		$this->assertContains(
 			Hookable_Mock::class,
-			Objects::get_property($registration_service, 'class_list')
+			Objects::get_property( $registration_service, 'class_list' )
 		);
 	}
 
 	/** @testdox It should be possible to pass custom rules to the Dependency Injection container to handle classes whos depenedencies cant be inferred. */
-	public function test_can_set_di_rule()
-	{
-		$app = (new App_Factory)
-			->with_wp_dice(true)
-			->di_rules(include FIXTURES_PATH . '/Application/dependencies.php')
+	public function test_can_set_di_rule() {
+		$app = ( new App_Factory )
+			->with_wp_dice( true )
+			->di_rules( include FIXTURES_PATH . '/Application/dependencies.php' )
 			->app();
 
-		$container = Objects::get_property($app, 'container');
-		$this->assertTrue($container->has(Interface_A::class));
+		$container = Objects::get_property( $app, 'container' );
+		$this->assertTrue( $container->has( Interface_A::class ) );
 	}
 
 	/** @testdox It should be possible to set custom settings to the apps config. */
-	public function test_can_set_config(): void
-	{
-		$app = (new App_Factory)
-			->with_wp_dice(true)
-			->app_config(include FIXTURES_PATH . '/Application/settings.php')
+	public function test_can_set_config(): void {
+		$app = ( new App_Factory )
+			->with_wp_dice( true )
+			->app_config( include FIXTURES_PATH . '/Application/settings.php' )
 			->app();
 
-		$app_config = Objects::get_property($app, 'app_config');
-		$this->assertEquals('test_value', $app_config->test_key);
+		$app_config = Objects::get_property( $app, 'app_config' );
+		$this->assertEquals( 'test_value', $app_config->test_key );
 	}
 
 	/** @testdox It should be possible to boot the app from a chained factory call. If no config is set, the defaults should be used. */
-	public function test_can_boot_app_from_factory_chain(): void
-	{
-		$app = (new App_Factory)
-			->with_wp_dice(true)
+	public function test_can_boot_app_from_factory_chain(): void {
+		$app = ( new App_Factory )
+			->with_wp_dice( true )
 			->boot();
-		$this->assertTrue($app::is_booted());
+		$this->assertTrue( $app::is_booted() );
 	}
 
 	/** @testdox It shoud be possble to pass the DI_Container interface as a depenedcy and have it populated with the current DI_Container implementation at initialisation.  */
-	public function test_di_container_rule_defined_at_init(): void
-	{
-		$app              = (new App_Factory)
-			->with_wp_dice(true)
+	public function test_di_container_rule_defined_at_init(): void {
+		$app              = ( new App_Factory )
+			->with_wp_dice( true )
 			->boot();
-		$has_di_container = $app::make(Has_DI_Container::class);
-		$this->assertTrue($has_di_container->di_set());
+		$has_di_container = $app::make( Has_DI_Container::class );
+		$this->assertTrue( $has_di_container->di_set() );
 	}
 
 	/** @testdox It should be possible to define additional registration middleware during the factory chained called. */
-	public function test_pass_registration_middleware_during_factory_init(): void
-	{
-		$mock_middleware = $this->createMock(Registration_Middleware::class);
+	public function test_pass_registration_middleware_during_factory_init(): void {
+		$mock_middleware = $this->createMock( Registration_Middleware::class );
 
-		$app = (new App_Factory)
-			->with_wp_dice(true)
-			->registration_middleware($mock_middleware)
+		$app = ( new App_Factory )
+			->with_wp_dice( true )
+			->registration_middleware( $mock_middleware )
 			->boot();
 
-		$registration    = Objects::get_property($app, 'registration');
-		$middleware_list = Objects::get_property($registration, 'middleware');
-		$this->assertContains($mock_middleware, $middleware_list);
+		$registration    = Objects::get_property( $app, 'registration' );
+		$middleware_list = Objects::get_property( $registration, 'middleware' );
+		$this->assertContains( $mock_middleware, $middleware_list );
 	}
 
 	/** @testdox It should be possible to defined additional registration middleware during the factory chained called, but as a class name, not as an instance. */
-	public function test_pass_registration_middleware_as_string_during_factory_init(): void
-	{
-		$app = (new App_Factory)
-			->with_wp_dice(true)
-			->construct_registration_middleware(Mock_Registration_Middleware::class)
+	public function test_pass_registration_middleware_as_string_during_factory_init(): void {
+		$app = ( new App_Factory )
+			->with_wp_dice( true )
+			->construct_registration_middleware( Mock_Registration_Middleware::class )
 			->boot();
 
-		$registration    = Objects::get_property($app, 'registration');
-		$middleware_list = Objects::get_property($registration, 'middleware');
-		$this->assertArrayHasKey(Mock_Registration_Middleware::class, $middleware_list);
+		$registration    = Objects::get_property( $app, 'registration' );
+		$middleware_list = Objects::get_property( $registration, 'middleware' );
+		$this->assertArrayHasKey( Mock_Registration_Middleware::class, $middleware_list );
 	}
 }
